550ca9dd77475f3d4fa6821274d08abdcf2cfd66
NickFromAIB <144272119+NickFromAIB@users.noreply.github.com>
2023-12-27 17:38:11 +0300

    Add files via upload

--- labs/Lab_12_python_intro/solution/3/3.py
+++ labs/Lab_12_python_intro/solution/3/3.py
@@ -1,20 +1,25 @@
-filename = input("Введите имя файла: ")
+def histogram(filename):
+    filename = input("Введите имя файла: ")
 
-with open(filename, 'r') as file:
-    str = file.read()
+    with open(filename, "r") as file:
+        str = file.read()
 
-char_count = {}
-maxh = 0
+    char_count = {}
+    maxh = 0
 
-for char in str:
-    if char == ' ':
-        continue
-    char_count[char] = char_count.get(char, 0) + 1
-    maxh = max(maxh, char_count[char])
+    for char in str:
+        if char == " ":
+            continue
+        char_count[char] = char_count.get(char, 0) + 1
+        maxh = max(maxh, char_count[char])
 
-unique_chars = sorted(char_count.keys())
+    unique_chars = sorted(char_count.keys())
 
-for i in range(maxh, 0, -1):
-    print(''.join('#' if char_count[tt] >= i else ' ' for tt in unique_chars))
+    for i in range(maxh, 0, -1):
+        print("".join("#" if char_count[tt] >= i else " " for tt in unique_chars))
 
-print(''.join(unique_chars))
+    print("".join(unique_chars))
+
+
+if __name__ == "__main__":
+    histogram(filename="")
